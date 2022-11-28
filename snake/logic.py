829a4a149584b3ef36038067709be89b34340e01
phpusr <phpusr@gmail.com>
2022-11-28 14:54:34 +0500

    Added Snake game

---
+++ snake/logic.py
@@ -0,0 +1,84 @@
+from queue import Queue
+import random
+from dataclasses import dataclass
+from enum import Enum
+
+
+class SnakeDirection(Enum):
+    up = 'U'
+    down = 'D'
+    left = 'L'
+    right = 'R'
+
+    @staticmethod
+    def can_change(old_direction, new_direction):
+        if old_direction is None or new_direction is None:
+            return False
+
+        access = { 
+            SnakeDirection.up: [SnakeDirection.left, SnakeDirection.right],
+            SnakeDirection.down: [SnakeDirection.left, SnakeDirection.right],
+            SnakeDirection.left: [SnakeDirection.up, SnakeDirection.down],
+            SnakeDirection.right: [SnakeDirection.up, SnakeDirection.down]
+        }
+        
+        return new_direction in access[old_direction]
+
+
+@dataclass
+class SnakeBlock:
+    xcor: int
+    ycor: int
+    direction: SnakeDirection
+    directions: list
+
+
+class Snake:
+    def __init__(self, field_width: int, field_height: int, direction: SnakeDirection = SnakeDirection.left):
+        self.prev_direction = SnakeDirection.left
+        self.direction = direction
+        self.speed = 1
+        self.current_block_index = 0
+        self.blocks = []
+        self.step = 0
+        padding = 5
+        start_xcor = random.randint(padding, field_width - padding)
+        start_ycor = random.randint(padding, field_height - padding)
+
+        for index in range(10):
+            self.blocks.append(SnakeBlock(
+                xcor=start_xcor + index,
+                ycor=start_ycor,
+                direction=self.direction,
+                directions=[]
+            ))
+
+    def change_direction(self, direction: SnakeDirection):
+        for block_index, block in enumerate(self.blocks):
+            block.directions.append((direction, self.step + 1 + block_index))
+
+    def move(self):
+        self.step += 1
+        
+        for block in self.blocks:
+            if len(block.directions) > 0:
+                direction, step = block.directions[0]
+                if self.step == step:
+                    block.directions.pop(0)
+                    block.direction = direction
+
+            if block.direction == SnakeDirection.up:
+                xdiff = 0
+                ydiff = -1
+            elif block.direction == SnakeDirection.down:
+                xdiff = 0
+                ydiff = 1
+            elif block.direction == SnakeDirection.left:
+                xdiff = -1
+                ydiff = 0
+            else:
+                xdiff = 1
+                ydiff = 0
+
+            block.xcor += xdiff
+            block.ycor += ydiff
---
+++ snake/main.py
@@ -0,0 +1,85 @@
+import sys
+
+import pygame
+
+from logic import Snake, SnakeDirection
+
+WIDTH_BLOCK_COUNT = 50
+HEIGHT_BLOCK_COUNT = 30
+BLOCK_WIDTH = 20
+MARGIN_BLOCK = 3
+
+WIDTH = 1000
+HEIGHT = 600
+FPS = 60
+
+SNAKE_COLOR = (200, 10, 10)
+
+
+class SnakeGame:
+    def __init__(self):
+        # Init data
+        self.prev_direction = SnakeDirection.left
+        self.current_direction = self.prev_direction
+        self.snake = Snake(WIDTH_BLOCK_COUNT, HEIGHT_BLOCK_COUNT, self.current_direction)
+        # Init GUI
+        pygame.init()
+        pygame.display.set_caption('Snake')
+        self.sc = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    def start(self):
+        clock = pygame.time.Clock()
+        self.restart()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        self.exit()
+                    elif event.key == pygame.K_r:
+                        self.restart()
+                    elif event.key == pygame.K_UP:
+                        self.current_direction = SnakeDirection.up
+                    elif event.key == pygame.K_DOWN:
+                        self.current_direction = SnakeDirection.down
+                    elif event.key == pygame.K_LEFT:
+                        self.current_direction = SnakeDirection.left
+                    elif event.key == pygame.K_RIGHT:
+                        self.current_direction = SnakeDirection.right
+            
+            self.change_direction()
+            self.snake.move()
+            self.draw_gui()
+            clock.tick(FPS)
+            pygame.time.wait(1000 - self.snake.speed * 100)
+
+    def restart(self):
+        self.current_direction = None
+        self.snake = Snake(WIDTH_BLOCK_COUNT, HEIGHT_BLOCK_COUNT)
+
+    def change_direction(self):
+        if SnakeDirection.can_change(self.prev_direction, self.current_direction):
+            self.snake.change_direction(self.current_direction)
+            self.prev_direction = self.current_direction
+
+    def draw_gui(self):
+        self.sc.fill((10, 30, 100))
+        self.draw_snake()
+        pygame.display.update()
+
+    def draw_snake(self):
+        block_index = 0
+        for block in self.snake.blocks:
+            xcor = block.xcor * BLOCK_WIDTH + (block.xcor + 1) * MARGIN_BLOCK
+            ycor = block.ycor * BLOCK_WIDTH + (block.ycor + 1) * MARGIN_BLOCK
+            pygame.draw.rect(self.sc, (200 - (block_index * 50) % 199, 100, 10), (xcor, ycor, BLOCK_WIDTH, BLOCK_WIDTH))
+            block_index += 1
+
+    def exit(self):
+        pygame.quit()
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    SnakeGame().start()
