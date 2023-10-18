234c18c444804f2326015fd93059f4029d12b770
phpusr <phpusr@gmail.com>
2023-10-18 22:25:57 +0500

    Minor edits

--- snake/logic.py
+++ snake/logic.py
@@ -2,8 +2,6 @@ import random
 from dataclasses import dataclass
 from enum import Enum
 
-from constants import WIDTH, HEIGHT
-
 
 class SnakeDirection(Enum):
     up = 'U'
@@ -16,13 +14,13 @@ class SnakeDirection(Enum):
         if old_direction is None or new_direction is None:
             return False
 
-        access = { 
+        access = {
             SnakeDirection.up: [SnakeDirection.left, SnakeDirection.right],
             SnakeDirection.down: [SnakeDirection.left, SnakeDirection.right],
             SnakeDirection.left: [SnakeDirection.up, SnakeDirection.down],
             SnakeDirection.right: [SnakeDirection.up, SnakeDirection.down]
         }
-        
+
         return new_direction in access[old_direction]
 
 
@@ -42,7 +40,7 @@ class Snake:
     def __init__(self, field_width: int, field_height: int, direction: SnakeDirection = SnakeDirection.left):
         self.prev_direction = SnakeDirection.left
         self.direction = direction
-        self.speed = 1
+        self.speed = 5
         self.current_block_index = 0
         self.blocks = []
         self.step = 0
@@ -69,7 +67,7 @@ class Snake:
 
     def move(self):
         self.step += 1
-        
+
         for block in self.blocks:
             if len(block.directions) > 0:
                 direction, step = block.directions[0]
@@ -97,9 +95,9 @@ class Snake:
         #TODO
         head = self.blocks[0]
         print(f'> head: {head.xcor}, {head.ycor}')
-        if head.xcor < self.start_field_xcor or head.xcor > self.end_field_xcor or \
-            head.ycor < self.start_field_xcor or head.ycor > self.end_field_xcor:
-                raise GameOverException()
+        if head.xcor < self.start_field_xcor or head.xcor > self.end_field_xcor \
+           or head.ycor < self.start_field_xcor or head.ycor > self.end_field_xcor:
+            raise GameOverException()
 
         # Block intersection check
         for index in range(len(self.blocks) - 1):
